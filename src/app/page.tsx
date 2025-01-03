fe12da6579e4201202071148d00e2fd5c82abd9b
Bismah Abdul Ghaffar <163390239+Bismah56@users.noreply.github.com>
2025-01-03 19:02:34 +0500

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-        <main className="bg-purple-500">
+    <main className="bg-purple-500">
       <ul className="flex list-none justify-between">
         <li><Link href="#">Home</Link></li>
         <li><Link href="#">Home</Link></li>
@@ -11,6 +11,7 @@ export default function Home() {
         <li><Link href="#">Home</Link></li>
         <li><Link href="#">Home</Link></li>
       </ul>
+      <Image src="/vercel.svg" alt="img" width={50} height={50}></Image>
     </main>
   );
 }
